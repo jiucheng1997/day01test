ddc76ad7a13b78587cb7df8b24616e29eb452cab
jiucheng1997 <1453260083@qq.com>
2020-04-21 16:05:13 +0800

    刘世辉

---
+++ src/main/java/cn/jiyun/pojo/Student.java
@@ -0,0 +1,13 @@
+package cn.jiyun.pojo;
+
+public class Student {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
